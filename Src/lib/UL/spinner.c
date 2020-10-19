970326c9a4e702c9cb0169653a442968bc869c0d
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-19 12:06:21 +0300

    add spinner header and source file

--- Src/lib/UL/spinner.c
+++ Src/lib/UL/spinner.c
@@ -5,4 +5,57 @@
  *      Author: yevhen.surkov
  */
 
+led_err_t led_enable(led_num_t e_Led_number)
+{
+	for (int var = 0; var < e_led_num; var++)
+	{
+		if (led_arr[var].num == e_Led_number)
+		{
+			led_add(&led_arr[var]);
+			led_arr[var].status = e_led_status_enable;
+			return e_err_ok ;
+		}
+	}
+	return e_err_not_found;
+}
 
+led_err_t led_disable(led_num_t e_LedNum)
+{
+	for (int var = 0; var < e_led_num; var++)
+	{
+		if (led_arr[var].num == e_LedNum)
+		{
+			led_remove(&led_arr[var]);
+			return e_err_ok;
+		}
+	}
+	return e_err_not_found;
+}
+
+led_err_t led_run(void)
+{
+	static int8_t count = 0;
+	if (led_arr[count].status != e_led_status_disable)
+	{
+	  if (e_led_on == led_arr[count].curr_state)
+	  {
+		  led_set_state(&led_arr[count], e_led_off);
+	  }
+	  else if (e_led_off == led_arr[count].curr_state)
+	  {
+		  led_set_state(&led_arr[count], e_led_on);
+	  }
+	  _Bool  state_changed = (led_arr[count].new_state != led_arr[count].curr_state);
+	  if (state_changed != 0)
+	  {
+		  HAL_GPIO_WritePin(led_arr[count].port, led_arr[count].pin,
+		    	(led_arr[count].new_state == e_led_on) ? SET : RESET);
+		  led_arr[count].curr_state = led_arr[count].new_state;
+	  }
+	}
+    if (count++ >= e_led_num)
+    {
+      count = 0;
+    }
+    return e_err_ok;
+}
--- Src/lib/UL/spinner.h
+++ Src/lib/UL/spinner.h
@@ -9,5 +9,23 @@
 #define SRC_LIB_UL_SPINNER_H_
 
 
+/// @file stm32f4xx_hal.h - Library that give access to the stm HAL level.
+#include "stm32f4xx_hal.h"
 
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+led_err_t led_run (void);
+
+led_err_t led_enable (led_num_t e_LedNum);
+
+led_err_t led_disable (led_num_t e_LedNum);
+
+
+#ifdef __cplusplus
+}
+#endif
 #endif /* SRC_LIB_UL_SPINNER_H_ */
