9a201f5752acc7ddde83ecedfd0e7452b1f4d0f4
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-02 01:20:23 +0200

    add void for queue
    add void for queue

--- Src/lib/utils/queue.c
+++ Src/lib/utils/queue.c
@@ -7,61 +7,83 @@
 
 #include "queue.h"
 
-#define MAX_SIZE 10
-
-static int8_t = size = MAX_SIZE;
+static int8_t = max_size = 0;
 static int8_t = curent_element_position = -1;
 static int8_t = last_element_position = -1;
 
+<<<<<<< Updated upstream
 static obj_led_t *led_que[size];
 
 que_err_t led_enque (obj_led_t *objP_this)
+=======
+static queue_struct_t obj_queue;
+
+que_err_t queue_create(void* thisP_array, uint32_t size, uint32_t quantity)
 {
-	if (last_element_position == size - 1)
-	{
-	    return e_que_err_que_is_full;
-	}
-	else
-	{
-		if (curent_element_position == -1)
-		{
-			curent_element_position = 0;
-		}
-		last_element_position++;
-		led_que[last_element_position] = objP_this;
-		return e_que_err_ok;
-	}
+    if(thisP_array != NULL && size != NULL && quantity != NULL)
+    {
+        max_size = size;
+        obj_queue = {thisP_array, size, quantity};
+        return e_que_err_ok;
+    }
+    return e_que_err_que_NULL_enter;
 }
 
+que_err_t led_enque(void *objP_this)
+>>>>>>> Stashed changes
+{
+    if(objP_this != NULL)
+    {
+        if (last_element_position == size - 1)
+        {
+            return e_que_err_que_is_full;
+        } else
+        {
+            if (curent_element_position == -1)
+            {
+                curent_element_position = 0;
+            }
+            last_element_position++;
+            obj_queue.P_array[last_element_position] = objP_this;
+            return e_que_err_ok;
+        }
+    }
+    return e_que_err_que_NULL_enter;
+}
+
+<<<<<<< Updated upstream
 obj_led_t led_deque (void)
+=======
+que_err_t led_deque(void /**objP_this*/)
+>>>>>>> Stashed changes
 {
-	 obj_led_t objP_this;
-	 if (curent_element_position == -1)
-	 {
-	    return NULL;
-	 }
-	 else
-	 {
-	    objP_this = *led_que[curent_element_position];
-		first_element_position++;
-	    if (curent_element_position > last_element_position)
-	    {
-	    	curent_element_position = last_element_position = -1;
-	    }
-	    return objP_this;
-	 }
+    obj_led_t objP_this;
+    if (curent_element_position == -1)
+    {
+        return NULL;
+    }
+    else
+    {
+        objP_this = *obj_queue.P_array[curent_element_position];
+        first_element_position++;
+        if (curent_element_position > last_element_position)
+        {
+            curent_element_position = last_element_position = -1;
+        }
+        return objP_this;
+    }
 }
 
-obj_led_t led_que_check (void)
+obj_led_t led_que_check(void)
 {
-	 obj_led_t objP_this;
-	 if (curent_element_position == -1)
-	 {
-	    return NULL;
-	 }
-	 else
-	 {
-	    objP_this = *led_que[curent_element_position];
-	    return objP_this;
-	 }
+    obj_led_t objP_this;
+    if (curent_element_position == -1)
+    {
+        return NULL;
+    }
+    else
+    {
+        objP_this = *obj_queue.P_array[curent_element_position];
+        return objP_this;
+    }
 }
--- Src/lib/utils/queue.h
+++ Src/lib/utils/queue.h
@@ -10,16 +10,28 @@
 
 typedef enum
 {
-  e_que_err_ok,
-  e_que_err_que_is_full,
-  e_que_err_que_is_empty,
-}que_err_t;
+    e_que_err_ok,
+    e_que_err_que_is_full,
+    e_que_err_que_is_empty,
+    e_que_err_que_NULL_enter,
+} que_err_t;
 
+<<<<<<< Updated upstream
+=======
+typedef struct
+{
+    void*    P_array;
+    uint32_t size;
+    uint32_t quantity;
+} queue_struct_t;
 
-que_err_t led_enque (led_queue_t *objP_this);
+que_err_t queue_create(void *P_array, uint32_t size, uint32_t quantity);
+>>>>>>> Stashed changes
 
-obj_led_t led_deque (void);
+que_err_t led_enque(void *objP_this);
 
-obj_led_t led_que_check (void);
+obj_led_t led_deque(void);
+
+obj_led_t led_que_check(void);
 
 #endif /* SRC_LIB_UTILS_QUEUE_H_ */
