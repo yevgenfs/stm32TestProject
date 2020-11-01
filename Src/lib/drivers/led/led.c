bc5546818660411ba33c927abd105a597593fb65
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-02 01:18:54 +0200

    add togler function
    add togler function

--- Src/lib/drivers/led/led.c
+++ Src/lib/drivers/led/led.c
@@ -13,36 +13,79 @@
 
 led_err_t led_add(obj_led_t *objP_this)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-//	init
-	GPIO_InitStruct.Pin = objP_this->pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    if (objP_this != NULL)
+    {
+        GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    //  init
+        GPIO_InitStruct.Pin = objP_this->pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    }
+    return e_led_err_NULL;
 }
 
 led_err_t led_remove(obj_led_t *objP_this)
 {
-	//led remove setup
+    //led remove setup
+    if (objP_this != NULL)
+    {
+        return e_led_err_ok;
+    }
+    return e_led_err_NULL;
 }
 
+<<<<<<< Updated upstream
 led_err_t led_init(void) {
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	return e_led_err_ok;
 
+=======
+led_err_t led_init(void) 
+{
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    return e_led_err_ok;
+>>>>>>> Stashed changes
 }
 
 led_err_t led_set_state(obj_led_t *objP_this, led_ctrl_t state)
 {
-	objP_this->new_state = state;
-	return e_led_err_ok;
+    if (objP_this != NULL)
+    {
+        objP_this->new_state = state;
+        return e_led_err_ok;
+    }
+    return e_led_err_NULL;
 }
 
+<<<<<<< Updated upstream
 led_err_t led_control(obj_led_t *objP_this, led_ctrl_t state)
 {
 	HAL_GPIO_WritePin(objP_this->port, objP_this->pin, (state) ? SET : RESET);
     return e_led_err_ok;
+=======
+led_err_t led_toggle(obj_led_t *objP_this)
+{
+    if (objP_this != NULL)
+    {
+        return(objP_this->curr_state == e_led_off) ? (objP_this->new_state = e_led_on) :
+                (objP_this->new_state = e_led_on);
+    }
+    return e_led_err_NULL;
+}
+
+led_err_t led_control(obj_led_t *objP_this, led_ctrl_t state)
+{
+    if (objP_this != NULL)
+    {
+        HAL_GPIO_WritePin(objP_this->port, objP_this->pin, (state) ? SET : RESET);
+        return e_led_err_ok;
+    }
+    return e_led_err_NULL;
+>>>>>>> Stashed changes
 }
 
--- Src/lib/drivers/led/led.h
+++ Src/lib/drivers/led/led.h
@@ -24,94 +24,101 @@ extern "C" {
 /// @brief led_err_t enum which express leds erorrs
 typedef enum
 {
-  e_led_err_ok,
-  e_led_err_not_found,
-}led_err_t;
+    e_led_err_ok,
+    e_led_err_not_found,
+    e_led_err_NULL
+} led_err_t;
 
 /// @brief led_ctrl_t enum which express leds state off or on
-typedef enum {
-	e_led_off ,
-	e_led_on
-}led_ctrl_t;
+typedef enum
+{
+    e_led_off,
+    e_led_on
+} led_ctrl_t;
 
 /// @brief led_status_t enum which express leds status disable or enable
-typedef enum {
-	e_led_status_disable ,
-	e_led_status_enable
-}led_status_t;
+typedef enum
+{
+    e_led_status_disable,
+    e_led_status_enable
+} led_status_t;
 
 /// @brief led_num_t enum which counts number of leds
-typedef enum {
-  e_led_num_1,
-  e_led_num_2,
-  e_led_num_3,
-  e_led_num_4,
-  e_led_num
-}led_num_t;
+typedef enum
+{
+    e_led_num_1,
+    e_led_num_2,
+    e_led_num_3,
+    e_led_num_4,
+    e_led_num
+} led_num_t;
 
 /// @brief obj_led_t struct which express instance of led
-typedef struct{
-	uint32_t      port;
-	uint16_t      pin;
-	led_num_t     num;
-	led_status_t  status;
-	led_ctrl_t    new_state;
-	led_ctrl_t    curr_state;
-}obj_led_t;
+typedef struct
+{
+    uint32_t     port;
+    uint16_t     pin;
+    led_num_t    num;
+    led_status_t status;
+    led_ctrl_t   new_state;
+    led_ctrl_t   curr_state;
+} obj_led_t;
 
 /**
-  @brief function which init leds
+ @brief function which init leds
 
-  @return return type of error or ok if work correctly
-*/
-led_err_t led_init (void);
-
-/**
-  @brief function which deinit leds
-
-  @return return type of error or ok if work correctly
-*/
-led_err_t led_deinit (void);
+ @return return type of error or ok if work correctly
+ */
+led_err_t led_init(void);
 
 /**
-  @brief function which turn off led or turn on led
+ @brief function which deinit leds
 
-  @param[in] objP_this instance of led which should   turn off or turn on
-
-  @param[in] state indicate which action to perfom on or off
-
-  @return return state of error or ok if work correctly
-*/
-led_err_t led_control (obj_led_t *objP_this, led_ctrl_t state);
+ @return return type of error or ok if work correctly
+ */
+led_err_t led_deinit(void);
 
 /**
-  @brief function which init led
+ @brief function which turn off led or turn on led
 
-  @param[in] objP_this instance of led which should  init
+ @param[in] objP_this instance of led which should   turn off or turn on
 
-  @return return type of error or ok if work correctly
-*/
+ @param[in] state indicate which action to perfom on or off
+
+ @return return state of error or ok if work correctly
+ */
+led_err_t led_control(obj_led_t *objP_this, led_ctrl_t state);
+
+/**
+ @brief function which init led
+
+ @param[in] objP_this instance of led which should  init
+
+ @return return type of error or ok if work correctly
+ */
 led_err_t led_add(obj_led_t *objP_this);
 
 /**
-  @brief function which deinit led
+ @brief function which deinit led
 
-  @param[in] objP_this instance of led which should  deinit
+ @param[in] objP_this instance of led which should  deinit
 
-  @return return type of error or ok if work correctly
-*/
+ @return return type of error or ok if work correctly
+ */
 led_err_t led_remove(obj_led_t *objP_this);
 
 /**
-  @brief function which change new_state of led instance
+ @brief function which change new_state of led instance
 
-  @param[in][out] objP_this instance of led which should set
+ @param[in][out] objP_this instance of led which should set
 
-  @param[in] state indicate which state should set on or off
+ @param[in] state indicate which state should set on or off
 
-  @return return type of error or ok if work correctly
-*/
+ @return return type of error or ok if work correctly
+ */
 led_err_t led_set_state(obj_led_t *objP_this, led_ctrl_t state);
+
+led_err_t led_toggle(obj_led_t *objP_this);
 
 #ifdef __cplusplus
 }
