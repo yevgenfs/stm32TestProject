6dac87fe416e59402733170d321bfaf332e57ecc
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-02 01:19:40 +0200

    add small state machine
    add small state machine

--- Src/lib/UL/spinner.h
+++ Src/lib/UL/spinner.h
@@ -8,7 +8,6 @@
 #ifndef SRC_LIB_UL_SPINNER_H_
 #define SRC_LIB_UL_SPINNER_H_
 
-
 /// @file stm32f4xx_hal.h - Library that give access to the stm HAL level.
 #include "stm32f4xx_hal.h"
 
@@ -20,6 +19,7 @@
 extern "C" {
 #endif
 
+<<<<<<< Updated upstream
 typedef enum {
 	e_spiner_work_mode_pause,
 	e_spiner_work_mode_run,
@@ -30,6 +30,39 @@ led_err_t spinner_init (void);
 led_err_t spinner_run (void);
 
 led_err_t spinner_deinit (void);
+=======
+typedef enum
+{
+    e_spiner_work_mode_pause = 0,
+    e_spiner_work_mode_run,
+} spiner_work_mode_t;
+
+typedef enum
+{
+    e_spiner_ctrl_insert = 0,
+    e_spiner_ctrl_remove
+} e_spiner_ctrl_t;
+
+typedef enum
+{
+    e_spinner_err_ok,
+    e_spinner_err_not_found,
+    e_spinner_err_not_NULL,
+    e_spinner_err_not_add_to_queue
+} e_spinner_err_t;
+
+typedef struct
+{
+    obj_led_t       *objP_led;
+    e_spiner_ctrl_t cmd;
+} spinner_ctrl_t;
+
+e_spinner_err_t spinner_init(void);
+
+e_spinner_err_t spinner_run(void);
+
+e_spinner_err_t spinner_deinit(void);
+>>>>>>> Stashed changes
 
 #ifdef __cplusplus
 }
