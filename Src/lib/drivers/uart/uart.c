1f87b069f674bbc0f21514a9dd0acf52d438c14f
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-12-15 14:18:55 +0200

    added null check
    added null check

--- Src/lib/drivers/uart/uart.c
+++ Src/lib/drivers/uart/uart.c
@@ -8,27 +8,30 @@
 
 uart_err_t uart_init(obj_uart_t* objP_this)
 {
-    if (objP_this == NULL)
+    if (objP_this != NULL && objP_this->uart_handler != NULL &&
+            objP_this->baud_rate !=0)
     {
-        return e_uart_err_init_fail;
+        objP_this->uart_handler->Instance           = USART2;
+        objP_this->uart_handler->Init.BaudRate      = objP_this->baud_rate;
+        objP_this->uart_handler->Init.WordLength    = UART_WORDLENGTH_8B;
+        objP_this->uart_handler->Init.StopBits      = UART_STOPBITS_1;
+        objP_this->uart_handler->Init.Parity        = UART_PARITY_NONE;
+        objP_this->uart_handler->Init.Mode          = UART_MODE_TX_RX;
+        objP_this->uart_handler->Init.HwFlowCtl     = UART_HWCONTROL_NONE;
+        objP_this->uart_handler->Init.OverSampling  = UART_OVERSAMPLING_16;
+
+        if (HAL_UART_Init(objP_this->uart_handler) != HAL_OK)
+        {
+          Error_Handler();
+          return e_uart_err_init_fail;
+        }
+
+        return e_uart_err_ok;
     }
 
-    objP_this->uart_handler->Instance           = USART2;
-    objP_this->uart_handler->Init.BaudRate      = objP_this->baud_rate;
-    objP_this->uart_handler->Init.WordLength    = UART_WORDLENGTH_8B;
-    objP_this->uart_handler->Init.StopBits      = UART_STOPBITS_1;
-    objP_this->uart_handler->Init.Parity        = UART_PARITY_NONE;
-    objP_this->uart_handler->Init.Mode          = UART_MODE_TX_RX;
-    objP_this->uart_handler->Init.HwFlowCtl     = UART_HWCONTROL_NONE;
-    objP_this->uart_handler->Init.OverSampling  = UART_OVERSAMPLING_16;
 
-    if (HAL_UART_Init(objP_this->uart_handler) != HAL_OK)
-    {
-      Error_Handler();
-      return e_uart_err_init_fail;
-    }
 
-    return e_uart_err_ok;
+    return e_uart_err_init_fail;
 }
 
 uart_err_t uart_deinit(obj_uart_t* objP_this)
@@ -43,11 +46,11 @@ uart_err_t uart_deinit(obj_uart_t* objP_this)
 
 uart_err_t uart_send(obj_uart_t* objP_this, uint8_t* str)
 {
-    if (objP_this == NULL && str == NULL)
+    if (objP_this == NULL && str == NULL
+            && objP_this->uart_handler == NULL)
     {
-        return e_uart_err_send_fail;
+        return (HAL_UART_Transmit_IT(objP_this->uart_handler, str,
+                strlen(str)) == HAL_OK) ? e_uart_err_ok : e_uart_err_send_fail;
     }
-
-    return (HAL_UART_Transmit_IT(objP_this->uart_handler, str, strlen(str)) == HAL_OK) ?
-            e_uart_err_ok : e_uart_err_send_fail;
+    return e_uart_err_send_fail;
 }
