edaa8458ffbfb01054042c6e0f209c0e8b316830
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2021-01-15 16:31:37 +0200

    add documentation

--- Src/lib/UL/uart_package_convertor/uart_package_convertor.h
+++ Src/lib/UL/uart_package_convertor/uart_package_convertor.h
@@ -14,19 +14,57 @@
 extern "C" {
 #endif
 
-/// @brief e_uart_manager_err_t enum which express uart_manager erorrs
+/// @brief e_uart_package_convertor_err_t enum which express uart_package_convertor erorrs
 typedef enum
 {
     e_uart_package_convertor_err_ok,
     e_uart_package_convertor_err_invalid_argument
 } e_uart_package_convertor_err_t;
 
+/**
+ @brief function which convert date that comes from uart
+
+ @param[in][out] receive_message data which come from uart
+
+ @param[in][out] receive_package expresses package that comes from uart
+
+ @param[in] array_size contain information about size of receive_message array  led state
+
+ @return return type of error or ok if work correctly
+*/
 e_uart_package_convertor_err_t convert_receive_package(uint8_t *receive_message, receive_package_t *receive_package, uint8_t array_size);
 
+/**
+ @brief function which convert data to header
+
+ @param[in] led_num indicate  which led send state
+
+ @param[in][out] send_package expresses package that will be sent to uart
+
+ @return return type of error or ok if work correctly
+*/
 e_uart_package_convertor_err_t convert_header(led_num_t led_num, send_package_t *send_package);
 
+/**
+ @brief function which data to payload
+
+ @param[in] led_state indicate  state of led (on/off)
+
+ @param[in][out] send_package expresses package that will be sent to uart
+
+ @return return type of error or ok if work correctly
+*/
 e_uart_package_convertor_err_t convert_payload(led_ctrl_t led_state, send_package_t *send_package);
 
+/**
+ @brief function which convert send_package to array
+
+ @param[in][out] send_message data which will be send to uart
+
+ @param[in] send_package expresses package that will be sent to uart
+
+ @return return type of error or ok if work correctly
+*/
 e_uart_package_convertor_err_t convert_message(uint8_t *send_message, send_package_t send_package);
 
 #ifdef __cplusplus
