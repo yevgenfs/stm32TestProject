f010976728f8e967665b1f20b09ccbacf4aacd08
Yevhen Surkov <yevhen.surkov@sigma.software>
2021-04-08 11:39:35 +0300

    addd Cmake to the project

---
+++ build/CMakeFiles/stm32_spinner_project-2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stm32_spinner_project-2.dir/Core/Src/main.c.obj"
+  "libstm32_spinner_project-2.dll.a"
+  "stm32_spinner_project-2.exe"
+  "stm32_spinner_project-2.exe.manifest"
+  "stm32_spinner_project-2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stm32_spinner_project-2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
